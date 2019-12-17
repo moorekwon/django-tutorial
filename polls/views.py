435b7a3f39a6a671dbfeef900e214ed2c521e9a3
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-17 17:41:26 +0900

    191217 show latest_question_list in index

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html')
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
