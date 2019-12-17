7b6784d708623d1dcf239c9780c67733e07f0713
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-17 17:09:39 +0900

    191217 language_code, time-zone setting

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from polls.models import Question, Choice
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
