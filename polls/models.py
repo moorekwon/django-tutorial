c889cb7b662f40f2f0200997d0bc33e90f8efa40
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-17 16:59:11 +0900

    191217 __str__ method definition fo question, choice

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,26 @@
+import datetime
+
 from django.db import models
-from django.db.models import Model
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        # 이 Question의 pub_date가 (현재시간 - 1일)보다 크거나 같은지 여부
+        # published 된 지 24시간 이내인지 여
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
